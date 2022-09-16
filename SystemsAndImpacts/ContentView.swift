d5df1bd370155728c7904736f1d36c621057e9dc
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-09-16 08:18:11 -0600

    Initial commit

---
+++ SystemsAndImpacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SystemsAndImpacts
+//
+//  Created by Mccarthy, Wallace on 9/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SystemsAndImpacts/Recources/SystemsAndImpactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemsAndImpactsApp.swift
+//  SystemsAndImpacts
+//
+//  Created by Mccarthy, Wallace on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SystemsAndImpactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
